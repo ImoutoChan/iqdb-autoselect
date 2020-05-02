37dee8f8bb77765f64ba76ba7f15a60d5cefca1e
ImoutoChan <kirirrin@gmail.com>
2020-05-02 14:57:48 +0300

    Add hotkey to save image silently
    Hotkey: CTRL+SHIFT(or ALT)+S

--- iqdb-search-hotkey.js
+++ iqdb-search-hotkey.js
@@ -1,23 +1,23 @@
 // ==UserScript==
 // @name         IqdbSearchHotKey
-// @version      1.3
+// @version      2.0
 // @description  Move mouse over image and press ctrl+q
 // @author       ImoutoChan
 //
 // @match        *://*/*
 //
-// @grant        GM.openInTab
 // @grant        GM_openInTab
+// @grant        GM_download
 // ==/UserScript==
 
 (function() {
     'use strict';
+    let hoverElem = null;
 
-    var hoverElem = null;
-
-    function rebind()     {
-        var imgs = document.querySelectorAll('img');
-        var as = document.querySelectorAll('a');
+    const rebind = () => {
+        const imgs = document.querySelectorAll('img');
+        const as = document.querySelectorAll('a');
+        const videos = document.querySelectorAll('video');
 
         Array.prototype.forEach.call(imgs, function(el, i){
             el.onmousemove = () => { hoverElem = el;};
@@ -25,19 +25,38 @@
         Array.prototype.forEach.call(as, function(el, i){
             el.onmousemove = () => { hoverElem = el; };
         });
+        Array.prototype.forEach.call(videos, function(el, i){
+            el.onmousemove = () => { hoverElem = el; };
+        });
+
+        if (videos.length === 1) {
+            hoverElem = videos[0];
+        }
+
+        if (imgs.length === 1) {
+            hoverElem = imgs[0];
+        }
     }
 
-    var observer = new MutationObserver(rebind);
+    const observer = new MutationObserver(rebind);
     observer.observe(document.body, { attributes: true, childList: true, subtree: true });
 
     rebind();
 
     document.addEventListener("keydown", function(e) {
         if (e.ctrlKey && (String.fromCharCode(e.which) === 'q' || String.fromCharCode(e.which) === 'Q')) {
-            var res = hoverElem.outerHTML.match(/https?:\/\/[^\/\s]+\/\S+\.(jpg|png|gif|jpeg|bmp)/i);
-            if (res !== null) {
-                var urlString = res[0];
+            const searchMatches = hoverElem.outerHTML.match(/https?:\/\/[^\/\s]+\/\S+\.(jpg|png|gif|jpeg|bmp)/i);
+            if (searchMatches !== null) {
+                const urlString = searchMatches[0];
                 GM_openInTab("http://iqdb.org/?url=" + encodeURIComponent(urlString), true);
+            }
+        } else if (e.ctrlKey && (e.altKey || e.shiftKey) && (String.fromCharCode(e.which) === 's' || String.fromCharCode(e.which) === 'S')) {
+            const saveMatches = hoverElem.outerHTML.match(/https?:\/\/[^\/\s]+\/\S+\.(jpg|png|gif|jpeg|bmp|webm|mp4)/i);
+            if (saveMatches !== null) {
+                const url = saveMatches[0];
+                const filename = url.substring(url.lastIndexOf('/') + 1);
+
+                GM_download({ url: url, name: filename, saveAs: false });
             }
         }
     });
