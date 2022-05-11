a6858d63ac567fa38efd6bc31c6c444f4effe1ea
ImoutoChan <kirirrin@gmail.com>
2022-05-11 23:18:31 +0300

    Prioritize size over sources for IqdbAutoselect

--- iqdb-autoselect.js
+++ iqdb-autoselect.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name         IqdbAutoselect
-// @version      1.2
-// @description  Auto select source site when found
+// @name         Iqdb Autoselect
+// @version      1.3
+// @description  Auto select best match when found
 // @author       ImoutoChan
-// @include      https://iqdb.org/*
-// @include      http://iqdb.org/*
+// @match        https://iqdb.org/*
+// @match        http://iqdb.org/*
 // @grant        none
 // ==/UserScript==
 
 function ready() {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-    autoselectDelay();
-  } else {
-    document.addEventListener('DOMContentLoaded', autoselectDelay);
-  }
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        autoselectDelay();
+    } else {
+        document.addEventListener('DOMContentLoaded', autoselectDelay);
+    }
 }
 
 function log(logtext) {
@@ -21,10 +21,10 @@ function log(logtext) {
 }
 
 function getPriority(source) {
-    switch(source) {
-        case "yande.re":
-            return 0;
+    switch (source) {
         case "danbooru":
+            return 0;
+        case "yande.re":
             return 1;
         case "sankaku":
             return 2;
@@ -46,11 +46,10 @@ function getPriority(source) {
     }
 }
 
-function autoselectDelay()
-{
-    setTimeout(function() {
+function autoselectDelay() {
+    setTimeout(function () {
         autoselect();
-    }, 5000);
+    }, 1000);
 }
 
 function autoselect() {
@@ -59,46 +58,61 @@ function autoselect() {
         return;
     }
 
-
     if (document.querySelector("#pages").innerHTML.indexOf("No relevant matches") >= 0) {
         log("not found");
         return;
     }
 
-    var entries = document.querySelectorAll("#pages > div > table > tbody");
+    const entries = document.querySelectorAll("#pages > div > table > tbody");
 
-    var foundEntries = [];
-    for(var i = 1; i < entries.length; i++) {
-        var current = entries[i];
+    const foundEntries = [];
+    for (let i = 1; i < entries.length; i++) {
+        const current = entries[i];
 
-        var source = current
+        const source = current
             .querySelector("tr:nth-child(3) > td")
             .textContent
             .split(' ')[0]
             .trim()
             .toLowerCase();
 
-        foundEntry = {
+        const foundEntry = {
             link: current.querySelector("tr > td > a").getAttribute("href"),
             source: source,
             similarity: current.querySelector("tr:nth-child(5) > td").innerHTML,
+            size: current.querySelector("tr:nth-child(4) > td").innerHTML,
             priority: getPriority(source)
         };
 
         foundEntries.push(foundEntry);
     }
 
-    foundEntries.sort(function(a, b) {
-        return a.priority - b.priority;
+    foundEntries.sort(function (a, b) {
+        const sizeRegex = /(?<width>\d+)×(?<height>\d+)/;
+
+        const aSize = a.size.match(sizeRegex).groups;
+        const bSize = b.size.match(sizeRegex).groups;
+
+        const higherASize = aSize.width > bSize.width && aSize.height > bSize.height;
+
+        if (higherASize)
+            return -100;
+
+        const higherBSize = bSize.width > aSize.width && bSize.height > aSize.height;
+        if (higherBSize)
+            return 100;
+
+        const higherPriority = a.priority - b.priority;
+        return higherPriority;
     });
 
-    for(i = 0; i < foundEntries.length; i++) {
-        log(foundEntries[i].source);
+    for (let i = 0; i < foundEntries.length; i++) {
+        log(JSON.stringify(foundEntries[i], null, 2));
     }
 
-    setTimeout(function() {
-        window.location = foundEntries[0].link;
-    }, 0);
+    setTimeout(function () {
+        //window.location = foundEntries[0].link;
+    }, 1);
 }
 
 ready();
