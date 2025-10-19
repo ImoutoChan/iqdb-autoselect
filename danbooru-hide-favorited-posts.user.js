4df92bcfdf00dacd64c8a7fdad758bfba808dc22
ImoutoChan <kirirrin@gmail.com>
2025-10-19 12:54:21 +0500

    Update version and improve button functionality

--- danbooru-hide-favorited-posts.user.js
+++ danbooru-hide-favorited-posts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Danbooru Hide Favs
-// @version      1.3
+// @version      1.4
 // @description  Add a button to hide faved images
 // @author       ImoutoChan
 // @match        https://danbooru.donmai.us/posts*
@@ -16,19 +16,39 @@
         Array.from(document.querySelectorAll('.post-unvote-link')).map(x => x.closest('article')).map(x => { x.style.display = x.style.display === 'none' ? 'block' : 'none'; return true; } );
     };
 
-    const appendButton = (caption) => {
-        const newButton = document.querySelector("#subnav-menu li:last-child").cloneNode(true);
-        newButton.style.float = 'right';
-
-        const newAnchor = newButton.childNodes[0];
-        newAnchor.innerHTML = caption;
-        newAnchor.setAttribute("href", "#");
-        newAnchor.onclick = func;
-        newAnchor.style.color = 'deeppink';
-
+    const appendButton = (caption, func) => {
         const menu = document.querySelector("#subnav-menu");
+        const firstRight = menu.querySelector(".ml-auto, .ms-auto");
+        const newButton = document.querySelector("#subnav-menu a:last-child").cloneNode(true);
+
+        newButton.textContent = caption;
+        newButton.href = "#";
+        newButton.onclick = func;
+        newButton.style.color = 'deeppink';
+
+        if (!firstRight) {
+            newButton.style.marginLeft = "auto";
+            newButton.classList.add("ml-auto");
+        }
+
         menu.appendChild(newButton);
+        cleanUp();
     };
 
-    appendButton('Hide Favs');
+    const cleanUp = () => {
+        const menu = document.querySelector("#subnav-menu");
+        const autos = Array.from(menu.querySelectorAll('.ml-auto, .ms-auto'));
+
+        let kept = false;
+        for (const el of autos) {
+            if (!kept) {
+                kept = true;
+            } else {
+                el.style.marginLeft = "unset";
+                el.classList.remove('ml-auto');
+            }
+        }
+    };
+
+    appendButton('Hide Favs', func);
 })();
