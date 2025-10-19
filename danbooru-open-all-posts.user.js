80c7e69523954bfce71a736c1852480426a8a51d
ImoutoChan <kirirrin@gmail.com>
2025-10-19 12:55:31 +0500

    Update version to 1.4 and improve button handling

--- danbooru-open-all-posts.user.js
+++ danbooru-open-all-posts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Danbooru Open All Posts
-// @version      1.3
+// @version      1.4
 // @description  Add a button to open all posts in tabs
 // @author       ImoutoChan
 // @match        https://danbooru.donmai.us/posts*
@@ -29,16 +29,39 @@
         }
     };
 
+    const appendButton = (caption, func) => {
+        const menu = document.querySelector("#subnav-menu");
+        const firstRight = menu.querySelector(".ml-auto, .ms-auto");
+        const newButton = document.querySelector("#subnav-menu a:last-child").cloneNode(true);
 
-    const newButton = document.querySelector("#subnav-menu li:last-child").cloneNode(true);
-    newButton.style.float = 'right';
+        newButton.textContent = caption;
+        newButton.href = "#";
+        newButton.onclick = func;
+        newButton.style.color = 'deeppink';
 
-    const newAnchor = newButton.childNodes[0];
-    newAnchor.innerHTML = 'Open All';
-    newAnchor.setAttribute("href", "#");
-    newAnchor.onclick = openAll;
-    newAnchor.style.color = 'deeppink';
+        if (!firstRight) {
+            newButton.style.marginLeft = "auto";
+            newButton.classList.add("ml-auto");
+        }
 
-    const menu = document.querySelector("#subnav-menu");
-    menu.appendChild(newButton);
+        menu.appendChild(newButton);
+        cleanUp();
+    };
+
+    const cleanUp = () => {
+        const menu = document.querySelector("#subnav-menu");
+        const autos = Array.from(menu.querySelectorAll('.ml-auto, .ms-auto'));
+
+        let kept = false;
+        for (const el of autos) {
+            if (!kept) {
+                kept = true;
+            } else {
+                el.style.marginLeft = "unset";
+                el.classList.remove('ml-auto');
+            }
+        }
+    };
+
+    appendButton("Open All", openAll);
 })();
